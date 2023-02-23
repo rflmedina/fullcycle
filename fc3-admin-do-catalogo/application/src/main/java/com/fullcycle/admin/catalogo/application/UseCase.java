f8bb0486ce3ffc63d034bfe196340eb2205eb303
Rafaela Medina <rafaelamedinax@gmail.com>
2023-02-23 15:06:35 -0300

    ...

--- fc3-admin-do-catalogo/application/src/main/java/com/fullcycle/admin/catalogo/application/UseCase.java
+++
@@ -1,7 +0,0 @@
-package com.fullcycle.admin.catalogo.application;
-
-public class UseCase {
-    public Category execute() {
-        System.out.println("");
-    }
-}
--- fc3-admin-do-catalogo/domain/src/main/java/com/fullcycle/admin/catalogo/domain/AggregateRoot.java
+++
@@ -1,7 +0,0 @@
-package com.fullcycle.admin.catalogo.domain;
-
-public class AggregateRoot<ID extends Identifier> extends Entity<ID> {
-    protected AggregateRoot(final ID id) {
-        super(id);
-    }
-}
